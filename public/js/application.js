eb62f840dd7e1a00777d32d66fbe1a1419a56244
Fangfei Li <email": null>
2017-04-29 20:10:55 -0700

    Add new fonts to each page and add content to homepage

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
+  $('#login-swap').on('click', function(){
+    $('#homepage-signup').hide();
+    $('#homepage-login').show();
+  });
 
+  $('#signup-swap').on('click', function(){
+    $('#homepage-login').hide();
+    $('#homepage-signup').show();
+  });
 });
